a62ecc2889e8c642f9945a41c8df2499be8ef2fa
Bianca Kendall <[D)>
2022-05-23 11:12:15 +0200

    created maven project in IntelliJ

---
+++ pokemon_project/src/main/java/com/bnta/pokemon_project/PokemonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.pokemon_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonProjectApplication.class, args);
+	}
+
+}
---
+++ pokemon_project/src/test/java/com/bnta/pokemon_project/PokemonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.pokemon_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
