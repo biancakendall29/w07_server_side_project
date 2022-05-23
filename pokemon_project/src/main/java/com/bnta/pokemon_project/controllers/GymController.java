5f18ae0a72ac985a7aafb5204e1f86d80323f635
Bianca Kendall <[D)>
2022-05-23 16:28:21 +0200

    added GET INDEX request on gym controller

--- pokemon_project/src/main/java/com/bnta/pokemon_project/controllers/GymController.java
+++ pokemon_project/src/main/java/com/bnta/pokemon_project/controllers/GymController.java
@@ -1,4 +1,27 @@
 package com.bnta.pokemon_project.controllers;
 
+import com.bnta.pokemon_project.models.Gym;
+import com.bnta.pokemon_project.repositories.GymRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("gyms")
 public class GymController {
+
+    @Autowired
+    private GymRepository gymRepository;
+
+    // INDEX
+    @GetMapping
+    public ResponseEntity<List<Gym>> getGyms() {
+        return new ResponseEntity(gymRepository.findAll(), HttpStatus.OK);
+    }
+
 }
