e5dd16a70af779f68c7307f5820797bfb595a584
bright-l-admin <bright-l-admin@BRIGHT-OS-24.lan>
2022-05-23 14:43:12 +0100

    Added Data to Gym, GymLeader & Pokemon Repos from DataLoader

--- pokemon_project/src/main/java/com/bnta/pokemon_project/components/DataLoader.java
+++ pokemon_project/src/main/java/com/bnta/pokemon_project/components/DataLoader.java
@@ -1,6 +1,6 @@
 package com.bnta.pokemon_project.components;
 
-import com.bnta.pokemon_project.models.Trainer;
+import com.bnta.pokemon_project.models.*;
 import com.bnta.pokemon_project.repositories.GymLeaderRepository;
 import com.bnta.pokemon_project.repositories.GymRepository;
 import com.bnta.pokemon_project.repositories.PokemonRepository;
@@ -37,10 +37,28 @@ public class DataLoader implements ApplicationRunner {
         Trainer trainer3 = new Trainer("Chase", LocalDate.of(2010,12,30));
         trainerRepository.saveAll(Arrays.asList(trainer1, trainer2, trainer3));
 
+        // Injecting Gyms into Gyms Repo
+        Gym gym1 = new Gym("Opelucid Gym");
+        Gym gym2 = new Gym("Driftveil Gym");
+        gymRepository.saveAll(Arrays.asList(gym1, gym2));
 
+        // Injecting the GymLeaders into GymLeader Repo
+        GymLeader gymLeader1 = new GymLeader("Iris", gym1);
+        GymLeader gymLeader2 = new GymLeader("Clay", gym2);
+        gymLeaderRepository.saveAll(Arrays.asList(gymLeader1, gymLeader2));
 
-
-
+        // Injecting the Pokemon into the Pokemon Repo
+        Pokemon pokemon1 = new Pokemon("Machamp", Type.FIGHTING, 50, trainer1);
+        Pokemon pokemon2 = new Pokemon("Torkoal", Type.FIRE, 10, trainer1);
+        Pokemon pokemon3 = new Pokemon("Treeko", Type.GRASS, 41, trainer1);
+        Pokemon pokemon4 = new Pokemon("Krabby", Type.WATER, 25, trainer2);
+        Pokemon pokemon5 = new Pokemon("Squirrel", Type.WATER, 78, trainer2);
+        Pokemon pokemon6 = new Pokemon("Chimchar", Type.FIRE, 97, trainer2);
+        Pokemon pokemon7 = new Pokemon("Klinglang", Type.STEEL, 53, trainer3);
+        Pokemon pokemon8 = new Pokemon("Alakazam", Type.PSYCHIC, 21, trainer3);
+        Pokemon pokemon9 = new Pokemon("Cinccino", Type.NORMAL, 15, trainer3);
+        Pokemon pokemon10 = new Pokemon("Bulbasaur", Type.GRASS, 46, trainer3);
+        pokemonRepository.saveAll(Arrays.asList(pokemon1,pokemon2,pokemon3,pokemon4,pokemon5,pokemon6,pokemon7,pokemon8,pokemon9,pokemon10));
     }
 
 
