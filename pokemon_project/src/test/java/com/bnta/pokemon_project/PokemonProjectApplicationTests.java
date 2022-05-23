318f50bbf5f2a7ce56898e625371a2cbd903bb24
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-05-23 12:05:54 +0100

    created add pokemon test

--- pokemon_project/src/test/java/com/bnta/pokemon_project/PokemonProjectApplicationTests.java
+++ pokemon_project/src/test/java/com/bnta/pokemon_project/PokemonProjectApplicationTests.java
@@ -10,4 +10,5 @@ class PokemonProjectApplicationTests {
 	void contextLoads() {
 	}
 
+
 }
--- pokemon_project/src/test/java/com/bnta/pokemon_project/models/TrainerTest.java
+++ pokemon_project/src/test/java/com/bnta/pokemon_project/models/TrainerTest.java
@@ -1,2 +1,29 @@
-package com.bnta.pokemon_project.models;public class TrainerTest {
+package com.bnta.pokemon_project.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class TrainerTest {
+
+    private Trainer trainer;
+    private Pokemon pokemon;
+    private Gym gym_badge;
+
+    @BeforeEach
+    public void setUp(){
+        trainer = new Trainer("Bianca", LocalDate.of(1999,3,14));
+        pokemon = new Pokemon("pikachu", Type.ELECTRIC,30);
+        gym_badge = new Gym("Pure Gym", new GymLeader("Naim", gym_badge));
+    }
+
+    @Test
+    public void canAddPokemon(){
+        trainer.addPokemon(pokemon);
+        assertThat(trainer.getPokemons().size()).isEqualTo(1);
+    }
+
 }
