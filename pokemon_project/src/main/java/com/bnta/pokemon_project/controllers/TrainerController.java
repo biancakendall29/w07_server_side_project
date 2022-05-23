57ad3946f62eab295380f65bbf513aba2c35529d
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-05-23 16:35:25 +0100

    added request mapping to trainer controller

--- pokemon_project/src/main/java/com/bnta/pokemon_project/controllers/TrainerController.java
+++ pokemon_project/src/main/java/com/bnta/pokemon_project/controllers/TrainerController.java
@@ -1,4 +1,48 @@
 package com.bnta.pokemon_project.controllers;
 
+import com.bnta.pokemon_project.models.Gym;
+import com.bnta.pokemon_project.models.Trainer;
+import com.bnta.pokemon_project.repositories.GymRepository;
+import com.bnta.pokemon_project.repositories.TrainerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("trainers")
 public class TrainerController {
+
+    @Autowired
+    private TrainerRepository trainerRepository;
+
+    // INDEX
+    @GetMapping
+    public ResponseEntity<List<Trainer>> getTrainer() {
+        return new ResponseEntity(trainerRepository.findAll(), HttpStatus.OK);
+    }
+
+    // SHOW
+    @GetMapping("/{id}")
+    public ResponseEntity<Trainer> getTrainer(@PathVariable Long id) {
+        var found = trainerRepository.findById(id);
+        return new ResponseEntity(trainerRepository.findById(id), found.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+    }
+
+    // POST
+    @PostMapping
+    public ResponseEntity<List<Trainer>> createTrainer(@RequestBody Trainer newTrainer) {
+        trainerRepository.save(newTrainer);
+        return new ResponseEntity(trainerRepository.findAll(), HttpStatus.CREATED);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<Trainer>> deleteTrainer(@PathVariable Long id) {
+        var found = trainerRepository.findById(id);
+        trainerRepository.deleteById(id);
+        return new ResponseEntity(trainerRepository.findAll(), found.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+    }
 }
