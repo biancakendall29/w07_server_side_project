5a26d1ee3c57d5e13799e2e242c5324ee18bd692
bright-l-admin <bright-l-admin@BRIGHT-OS-24.lan>
2022-05-24 11:14:10 +0100

    Added in GymRepository Derived Queries & Test

--- pokemon_project/src/test/java/com/bnta/pokemon_project/PokemonProjectApplicationTests.java
+++ pokemon_project/src/test/java/com/bnta/pokemon_project/PokemonProjectApplicationTests.java
@@ -1,14 +1,55 @@
 package com.bnta.pokemon_project;
 
+import com.bnta.pokemon_project.models.GymLeader;
+import com.bnta.pokemon_project.repositories.GymLeaderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class PokemonProjectApplicationTests {
+
+@Autowired
+GymLeaderRepository gymLeaderRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	// Naim's Derived Queries Tests
+
+	// DQ 1 & 2 Test - FindByName (Case & Non-Case Sensitive - only tests non-sensitive query)
+	@Test
+	public void canFindNameContainingWord () {
+		List<GymLeader> found = gymLeaderRepository.findByNameContainingIgnoreCase("clay");
+		assertThat(found.size()).isEqualTo(1);
+	}
+
+	// DQ 3 - FindByGym
+	@Test
+	public void canFindByGym () {
+		List<GymLeader> found = gymLeaderRepository.findByGym("Opelucid");
+		assertThat(found.size()).isEqualTo(1);
+	}
+
+	// DQ4 - Need to research more on this
+/*
+	@Test
+	public void canFindByID () {
+		List<GymLeader> found = gymLeaderRepository.findById(""); // Pass something through brackets
+		assertThat(found.size()).isEqualTo(1);
+	}
+*/
+
+	// DQ5
+	@Test
+	public void canFindNameContaining (){
+		List<GymLeader> found = gymLeaderRepository.findByNameContainingIgnoreCase("cl");
+		assertThat(found.size()).isEqualTo(1);
+	}
 
 }
