003a7b5fee484da7ab49daa56fb5b1d83770ba49
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-05-24 10:11:12 +0100

    created derived query test for gym

--- pokemon_project/src/test/java/com/bnta/pokemon_project/PokemonProjectApplicationTests.java
+++ pokemon_project/src/test/java/com/bnta/pokemon_project/PokemonProjectApplicationTests.java
@@ -1,14 +1,33 @@
 package com.bnta.pokemon_project;
 
+import com.bnta.pokemon_project.models.Gym;
+import com.bnta.pokemon_project.repositories.GymRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class PokemonProjectApplicationTests {
 
+//	Amber's DQ testing
+	@Autowired
+	GymRepository gymRepository;
+
 	@Test
 	void contextLoads() {
 	}
+
+//	Amber's DQ testing
+	@Test
+	public void canFindNameContainingWord(){
+		List<Gym> found = gymRepository.findByNameContainingIgnoreCase("driftveil");
+		assertThat(found.size()).isEqualTo(1);
+	}
+//	todo: need to add more gyms as there is not much to test on
 
 
 }
