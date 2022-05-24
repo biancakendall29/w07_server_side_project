2f2a2a7c4938cffd89c67e5f1a7e842b387bdf88
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-05-24 11:30:15 +0100

    added Naim's derived queries

--- pokemon_project/src/main/java/com/bnta/pokemon_project/repositories/GymLeaderRepository.java
+++ pokemon_project/src/main/java/com/bnta/pokemon_project/repositories/GymLeaderRepository.java
@@ -1,28 +1,26 @@
 package com.bnta.pokemon_project.repositories;
 
-import com.bnta.pokemon_project.models.Gym;
 import com.bnta.pokemon_project.models.GymLeader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GymLeaderRepository extends JpaRepository <GymLeader, Long> {
-    // Amber's lines
+
+    // Naim's DQ
+    // NA DQ1 - Find GymLeader by Name (case-sensitive)
+    List<GymLeader> findByName (String name);
+
+    // NA DQ2 - Find GymLeader by Name (not case-sensitive)
+    List<GymLeader> findByNameContainingIgnoreCase(String Name);
 
 
+    // NA DQ3 - Find GymLeader by Gym
+    List<GymLeader> findByGym (String gym);
 
+    // NA DQ4 - Find any GymLeader whose name contains the string 'Cl' within it's name
+    List<GymLeader> findByNameContaining (String string);
+    // Double Check This
 
-    //
-
-    // Naim's lines
-
-
-
-
-    //
-
-    // Bianca's lines
-
-
-
-
-    //
 }
